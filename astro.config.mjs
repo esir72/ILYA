164e0991313ea4e6f33371fc9b4cf50baaf17f8d
esir72 <saloma.111.1.11@yandex.ru>
2026-09-05 13:35:04 +0500

    fix: update base path to /ILYA for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://72esir.github.io',
-  base: '/ilya-lending',
+  base: '/ILYA',
 });
